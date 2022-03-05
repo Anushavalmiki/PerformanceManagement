589b767b767a7f63d70e4f032467eb5cb07e49c4
Anusha <anushavalmiki.amaze@gmail.com>
2022-03-05 15:06:29 +0530

    aaaaaaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxDropzoneModule } from 'ngx-dropzone';
@@ -91,6 +91,7 @@ import { LoaderComponent } from './loader/loader.component';
     NgxPaginationModule,
     NgMultiSelectDropDownModule,
     CKEditorModule,
+    ReactiveFormsModule
     
   ],
   providers: [DatePipe ],
